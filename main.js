7de2c39042108e184ecc7bfa38d0fb8a3390f151
Igorth <igordiasth@gmail.com>
2024-02-01 19:28:13 -0500

    feat: add rest operator with objects and array

--- main.js
+++ main.js
@@ -9,15 +9,15 @@
 
 // Objetos
 
-const user = {
-  name: 'John',
-  nickname: 'Dier',
-  age: 30,
-  address: {
-    city: 'New York',
-    state: 'NY',
-  },
-};
+// const user = {
+//   name: 'John',
+//   nickname: 'Dier',
+//   age: 30,
+//   address: {
+//     city: 'New York',
+//     state: 'NY',
+//   },
+// };
 
 // document.body.innerText = 'name' in user;
 // document.body.innerText = Object.keys(user);
@@ -30,10 +30,36 @@ const user = {
 // const address = user.address;
 // document.body.innerText = JSON.stringify(address);
 
-const { name: firstName, address, nickname = 'Igor' } = user;
+// const { name: firstName, address, nickname = 'Igor' } = user;
 // document.body.innerText = JSON.stringify({ firstName, address, nickname });
 
-function showAge({ age }) {
-  return age;
-}
-document.body.innerText = JSON.stringify(showAge(user));
+// function showAge({ age }) {
+//   return age;
+// }
+// document.body.innerText = JSON.stringify(showAge(user));
+
+////////////////////////////////////////////////////////////////
+
+// REST OPERATOR
+
+const user = {
+  name: 'John',
+  nickname: 'Dier',
+  age: 30,
+  address: {
+    city: 'New York',
+    state: 'NY',
+  },
+};
+
+// const { name, nickname, ...rest } = user;
+// document.body.innerText = JSON.stringify(rest);
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const [first, , third, ...rest] = array;
+
+console.log(JSON.stringify({ first, third }));
+document.body.innerText = JSON.stringify(rest);
+document.body.innerText = JSON.stringify({ first, third, rest });
+
+////////////////////////////////////////////////////////////////
