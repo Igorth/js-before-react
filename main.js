f727e0570fdd7b4dc473520d96046c34ee45ec3a
Igorth <igordiasth@gmail.com>
2024-02-01 19:16:11 -0500

    feat: add objects methods
    This commit introduce Objects and Destructing  methods related to Objects Methods

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 // Nullish coalescing operator.
 
-const age = 0;
+// const age = 0;
 
-// 0, '', [], false, undefined, null => falsy
-document.body.innerText = `Your age is ${age ?? 'Não Informado'} years old`;
+// // 0, '', [], false, undefined, null => falsy
+// document.body.innerText = `Your age is ${age ?? 'Não Informado'} years old`;
+
+////////////////////////////////////////////////////////////////
+
+// Objetos
+
+const user = {
+  name: 'John',
+  nickname: 'Dier',
+  age: 30,
+  address: {
+    city: 'New York',
+    state: 'NY',
+  },
+};
+
+// document.body.innerText = 'name' in user;
+// document.body.innerText = Object.keys(user);
+// document.body.innerText = Object.values(user);
+// document.body.innerText = JSON.stringify(Object.values(user));
+// document.body.innerText = JSON.stringify(Object.entries(user));
+
+// DESESTRUTURAÇÃO
+
+// const address = user.address;
+// document.body.innerText = JSON.stringify(address);
+
+const { name: firstName, address, nickname = 'Igor' } = user;
+// document.body.innerText = JSON.stringify({ firstName, address, nickname });
+
+function showAge({ age }) {
+  return age;
+}
+document.body.innerText = JSON.stringify(showAge(user));
